9211862854750d48fcaded4bc4eb706dcb2ca307
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-09-04 20:38:18 +0300

    Update coordinates on every change of city value

--- lib/page/SettingsPage.dart
+++ lib/page/SettingsPage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_forecast/api/OpenWeatherMapApi.dart';
+import 'package:weather_forecast/datamodel/Coordinates.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -18,6 +21,27 @@ class _SettingsPageState extends State<SettingsPage> {
           title: 'City',
           settingKey: 'city',
           initialValue: 'London',
+          onChange: (String value) async {
+            Coordinates coordinates = await fetchCoordinates(http.Client());
+            Settings.setValue<String>('lan', coordinates.lan.toString());
+            Settings.setValue<String>('lon', coordinates.lon.toString());
+          },
+          borderColor: Theme.of(context).accentColor,
+          errorColor: Theme.of(context).errorColor,
+        ),
+        TextInputSettingsTile(
+          title: 'Longitude',
+          settingKey: 'lon',
+          keyboardType:
+              TextInputType.numberWithOptions(signed: true, decimal: true),
+          borderColor: Theme.of(context).accentColor,
+          errorColor: Theme.of(context).errorColor,
+        ),
+        TextInputSettingsTile(
+          title: 'Latitude',
+          settingKey: 'lan',
+          keyboardType:
+              TextInputType.numberWithOptions(signed: true, decimal: true),
           borderColor: Theme.of(context).accentColor,
           errorColor: Theme.of(context).errorColor,
         ),
