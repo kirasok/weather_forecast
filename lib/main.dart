0c7c319b82105f25b0fb84eceba04d52202a80a9
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-09-04 19:38:53 +0300

    Implement blank SettingsScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 
 import 'app/MainApp.dart';
 
-void main() {
+void main() async {
+  await Settings.init();
   runApp(MainApp());
 }
