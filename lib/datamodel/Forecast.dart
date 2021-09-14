306c722e5933d0ef7994cbff4a56a55b30262d5b
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-09-14 10:54:56 +0300

    Set HiveField for each variable

--- lib/datamodel/Forecast.dart
+++ lib/datamodel/Forecast.dart
@@ -2,14 +2,22 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 @HiveType(typeId: 0)
 class Forecast extends HiveObject {
+  @HiveField(0)
   final double lat;
+  @HiveField(1)
   final double lon;
+  @HiveField(2)
   final String timezone;
+  @HiveField(3)
   final int timezone_offset;
 
+  @HiveField(4)
   final Current current;
+  @HiveField(5)
   final List<Minutely> minutely;
+  @HiveField(6)
   final List<Hourly> hourly;
+  @HiveField(7)
   final List<Daily> daily;
 
   Forecast({
@@ -26,9 +34,13 @@ class Forecast extends HiveObject {
 
 @HiveType(typeId: 1)
 class Weather {
+  @HiveField(0)
   final int id;
+  @HiveField(1)
   final String main;
+  @HiveField(2)
   final String description;
+  @HiveField(3)
   final String icon;
 
   Weather({
@@ -41,20 +53,35 @@ class Weather {
 
 @HiveType(typeId: 2)
 class Current {
+  @HiveField(0)
   final int dt;
+  @HiveField(1)
   final double temp;
+  @HiveField(2)
   final double feels_like;
+  @HiveField(3)
   final double pressure;
+  @HiveField(4)
   final double humidity;
+  @HiveField(5)
   final double dew_point;
+  @HiveField(6)
   final double uvi;
+  @HiveField(7)
   final double clouds;
+  @HiveField(8)
   final double visibility;
+  @HiveField(9)
   final double wind_speed;
+  @HiveField(10)
   final double wind_gust; // Where available
+  @HiveField(11)
   final double wind_deg;
+  @HiveField(12)
   final double rain; // Where available
+  @HiveField(13)
   final double snow; // Where available
+  @HiveField(14)
   final Weather weather;
 
   Current({
@@ -78,7 +105,9 @@ class Current {
 
 @HiveType(typeId: 3)
 class Minutely {
+  @HiveField(0)
   final int dt;
+  @HiveField(1)
   final double precipitation;
 
   Minutely({
@@ -89,7 +118,9 @@ class Minutely {
 
 @HiveType(typeId: 4)
 class Hourly {
+  @HiveField(0)
   final Current current;
+  @HiveField(1)
   final double pop;
 
   Hourly({
@@ -100,11 +131,17 @@ class Hourly {
 
 @HiveType(typeId: 5)
 class Temp {
+  @HiveField(0)
   final double morn;
+  @HiveField(1)
   final double day;
+  @HiveField(2)
   final double eve;
+  @HiveField(3)
   final double night;
+  @HiveField(4)
   final double min;
+  @HiveField(5)
   final double max;
 
   Temp({
@@ -119,9 +156,13 @@ class Temp {
 
 @HiveType(typeId: 6)
 class Feels_like {
+  @HiveField(0)
   final double morn;
+  @HiveField(1)
   final double day;
+  @HiveField(2)
   final double eve;
+  @HiveField(3)
   final double night;
 
   Feels_like({
@@ -134,24 +175,45 @@ class Feels_like {
 
 @HiveType(typeId: 7)
 class Daily {
+  @HiveField(0)
   final int dt;
+  @HiveField(1)
   final int sunrise;
+  @HiveField(2)
   final int sunset;
+  @HiveField(3)
   final int moonrise;
+  @HiveField(4)
   final int moonset;
+  @HiveField(5)
   final double moon_phase;
+  @HiveField(6)
   final Temp temp;
+  @HiveField(7)
   final Feels_like feels_like;
+  @HiveField(8)
   final double pressure;
+  @HiveField(9)
   final double humidity;
+  @HiveField(10)
   final double dew_point;
+  @HiveField(11)
   final double wind_speed;
+  @HiveField(12)
   final double wind_gust; // Where available
-  final double wind_deg, clouds;
+  @HiveField(13)
+  final double wind_deg;
+  @HiveField(14)
+  final double clouds;
+  @HiveField(15)
   final double uvi;
+  @HiveField(16)
   final double pop;
+  @HiveField(17)
   final double rain; // Where available
+  @HiveField(18)
   final double snow; // Where available
+  @HiveField(19)
   final Weather weather;
 
   Daily({
