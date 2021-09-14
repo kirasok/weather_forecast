9f78ab0c1f4dd79b7b579ff1e5154f438a42b740
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-09-14 09:57:53 +0300

    Init hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'app/MainApp.dart';
 
 void main() async {
   await Settings.init();
+  await Hive.initFlutter();
   runApp(MainApp());
 }
