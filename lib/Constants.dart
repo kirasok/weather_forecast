683daf11b0a378dd92edfa0609157730e471127f
Kira Sokolova <Kyra256@proton.me>
2021-09-14 09:59:55 +0300

    Open hive box

---
+++ lib/Constants.dart
@@ -0,0 +1,3 @@
+abstract class Constants {
+  static const forecast_box = "forecast_box";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:weather_forecast/Constants.dart';
 
 import 'app/MainApp.dart';
 
 void main() async {
   await Settings.init();
   await Hive.initFlutter();
+  await Hive.openBox(Constants.forecast_box);
   runApp(MainApp());
 }
