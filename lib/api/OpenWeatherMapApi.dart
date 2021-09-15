d3126cf7a52398c8a6b13df11419a8c726523c11
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-09-15 12:56:41 +0300

    Add api to fetch forecast

--- lib/api/OpenWeatherMapApi.dart
+++ lib/api/OpenWeatherMapApi.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather_forecast/datamodel/Coordinates.dart';
+import 'package:weather_forecast/datamodel/Forecast.dart';
 
 Future<Coordinates> fetchCoordinates(http.Client client) async {
   String city = Settings.getValue<String>('city', 'London');
@@ -22,4 +23,25 @@ Future<Coordinates> fetchCoordinates(http.Client client) async {
 
 Coordinates parseCoordinates(String responseBody) {
   return Coordinates.fromJson(jsonDecode(responseBody));
+}
+
+Future<Forecast> fetchForecast(http.Client client) async {
+  String lat = Settings.getValue('lat', '');
+  String lon = Settings.getValue('lon', '');
+  String key = Settings.getValue<String>('api-key', '');
+
+  final response = await client.get(
+    Uri.parse(
+        'https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&exclude=alerts&appid=$key'),
+  );
+
+  if (response.statusCode == 200) {
+    return parseForecast(response.body);
+  } else {
+    throw Exception('Failed to load forecast');
+  }
+}
+
+Forecast parseForecast(String responseBody) {
+  return Forecast.fromJson(jsonDecode(responseBody));
 }
