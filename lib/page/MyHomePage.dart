a8c5d9a01e8563487ae4c54cc45d0931b31c1443
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-09-15 17:42:16 +0300

    Add basic FutureBuilder

--- lib/page/MyHomePage.dart
+++ lib/page/MyHomePage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather_forecast/api/OpenWeatherMapApi.dart';
+import 'package:weather_forecast/datamodel/Forecast.dart';
 
 import 'SettingsPage.dart';
 
@@ -15,91 +15,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late Future<Forecast> forecast;
+
   @override
   void initState() {
     super.initState();
 
-    fetchForecast(http.Client());
+    forecast = fetchForecast(http.Client());
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SettingsPage(),
-                ),
-              );
-            },
-            icon: Icon(Icons.settings),
-          ),
-        ],
-      ),
-      body: StaggeredGridView.countBuilder(
-        crossAxisCount: 4,
-        itemCount: _widgets.length,
-        itemBuilder: (BuildContext context, int index) => Container(
-          decoration: BoxDecoration(
-              color: Colors.grey.shade200,
-              borderRadius: BorderRadius.circular(54.0)),
-          child: Center(
-            child: _widgets[index],
-          ),
-        ),
-        staggeredTileBuilder: (int index) =>
-            new StaggeredTile.count(2, index % 3 == 0 ? 2 : 1),
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 4.0,
-        padding: EdgeInsets.all(8),
-      ),
-    );
-  }
-
-  List<Widget> _widgets = <Widget>[
-    // Temperature
-    Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('+14'),
-            Icon(Icons.access_alarm),
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SettingsPage(),
+                  ),
+                );
+              },
+              icon: Icon(Icons.settings),
+            ),
           ],
         ),
-        Text('Real feel +13'),
-        Text('Updated 19:04'),
-      ],
-    ),
-    // Wind
-    Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Icon(Icons.arrow_back_rounded),
-        Text('4 m/s'),
-        Text('Gusts 9 m/s'),
-      ],
-    ),
-    // Precipitation
-    Text('4 mm'),
-    // Pressure
-    Text('1000 hPa'),
-    // Humidity
-    Text('90%'),
-    // Clouds
-    Text('100%'),
-    // UV-index
-    Text('0'),
-    // Visibility
-    Text('10 km'),
-  ];
+        body: FutureBuilder<Forecast>(
+          future: forecast,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data!.current.feels_like.toString());
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+
+            return const CircularProgressIndicator();
+          },
+        ));
+  }
 }
