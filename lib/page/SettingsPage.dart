425fada47982e4ad2b111700ddfa9292778f15d2
Kira Sokolova <Kyra256@proton.me>
2021-09-16 10:00:53 +0300

    Update forecast if changed location

--- lib/page/SettingsPage.dart
+++ lib/page/SettingsPage.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather_forecast/api/OpenWeatherMapApi.dart';
+import 'package:weather_forecast/database/HiveUtils.dart';
 import 'package:weather_forecast/datamodel/Coordinates.dart';
+import 'package:weather_forecast/datamodel/Forecast.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -26,6 +28,8 @@ class _SettingsPageState extends State<SettingsPage> {
             Settings.setValue<String>('lat', coordinates.lat.toString());
             Settings.setValue<String>('lon', coordinates.lon.toString());
             setState(() {});
+            Forecast forecast = await fetchForecast(http.Client());
+            putForecast(forecast);
           },
           borderColor: Theme.of(context).colorScheme.secondary,
           errorColor: Theme.of(context).errorColor,
