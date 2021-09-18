080e841ec9bd8d5c46608fcf2c9f380b42273e7f
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-09-18 12:43:36 +0300

    Find System locale

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:intl/intl_browser.dart';
 import 'package:weather_forecast/Constants.dart';
 import 'package:weather_forecast/datamodel/Forecast.dart';
 
@@ -11,6 +12,7 @@ void main() async {
   await Hive.initFlutter();
   await registerAdapters();
   await Hive.openBox(Constants.forecast_box);
+  await findSystemLocale();
   runApp(MainApp());
 }
 
