e956e3325ff0b5a98b58ffcd8a9af4e1254c39ea
Kira Sokolova <Kyra256@proton.me>
2021-10-02 15:56:37 +0300

    Add widgets to HourlyListTile

--- lib/widget/HourlyListTile.dart
+++ lib/widget/HourlyListTile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather_forecast/datamodel/Forecast.dart';
 import 'package:weather_forecast/utils/Utils.dart';
+import 'package:weather_icons/weather_icons.dart';
 
 class HourlyListTile extends StatelessWidget {
   final Hourly hourly;
@@ -12,15 +13,58 @@ class HourlyListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Image.network(
-          'https://openweathermap.org/img/wn/${hourly.current.weather.icon}@2x.png'),
-      title: Text(
-        getDateTime(
-          DateTime.fromMillisecondsSinceEpoch(
-            hourly.current.dt * 1000,
-            isUtc: true,
-          ),
+    double precipitation = 0;
+    if (hourly.current.rain > 0) precipitation += hourly.current.rain;
+    if (hourly.current.snow > 0) precipitation += hourly.current.snow;
+
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(32, 8, 0, 16),
+        child: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
+                  child: Text(
+                    getDateTime(
+                      DateTime.fromMillisecondsSinceEpoch(
+                          hourly.current.dt * 1000,
+                          isUtc: true),
+                    ),
+                    style: Theme.of(context).textTheme.overline,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      getTemp(hourly.current.temp),
+                    ),
+                    WindIcon(
+                      degree: hourly.current.wind_deg,
+                    ),
+                    Text(getSpeed(hourly.current.wind_speed)),
+                  ],
+                ),
+                Text(
+                  'Pressure: ' + getPressure(hourly.current.pressure),
+                ),
+                Text('Humidity: ' + toPercentage(hourly.current.humidity)),
+                Text('Clouds: ' + toPercentage(hourly.current.clouds)),
+                Text('Probability of precipitation: ' +
+                    toPercentage(hourly.pop)),
+                Text('Last hour precipitation: $precipitation mm'),
+              ],
+            ),
+            Expanded(
+              child: Image.network(
+                'https://openweathermap.org/img/wn/${hourly.current.weather.icon}@2x.png',
+              ),
+            ),
+          ],
         ),
       ),
     );
