2c48348b03ebb36691fb3c7a867f5d570ae24ddf
Kira Sokolova <Kyra256@proton.me>
2021-10-02 14:25:14 +0300

    Change colorscheme of the app

--- lib/app/MainApp.dart
+++ lib/app/MainApp.dart
@@ -4,17 +4,21 @@ import 'package:weather_forecast/page/MyHomePage.dart';
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context).copyWith(
+      colorScheme: Theme.of(context).colorScheme.copyWith(
+            primary: Colors.lightBlue.shade800,
+            secondary: Colors.deepOrangeAccent.shade700,
+          ),
+      cardTheme: CardTheme.of(context).copyWith(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+        elevation: 5,
+        margin: EdgeInsets.all(12),
+      ),
+    );
+
     return MaterialApp(
       title: 'Weather Forecast',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        cardTheme: CardTheme(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
-          elevation: 5,
-          margin: EdgeInsets.all(12),
-        ),
-      ),
+      theme: theme,
       home: MyHomePage(title: 'Weather'),
     );
   }
