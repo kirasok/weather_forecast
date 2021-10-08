2e497f78a6aafbb4604e8b77a6d621d394b21df6
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-10-08 13:56:58 +0300

    Refactor HiveUtils for cleaner code

--- lib/database/HiveUtils.dart
+++ lib/database/HiveUtils.dart
@@ -1,9 +1,15 @@
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_forecast/api/OpenWeatherMapApi.dart';
 import 'package:weather_forecast/datamodel/Forecast.dart';
 
 import '../Constants.dart';
 
-void putForecast(Forecast forecast) {
+Future<void> putForecast(Forecast forecast) async {
   Hive.box(Constants.forecast_box)
       .put(DateTime.now().toUtc().toIso8601String(), forecast);
+}
+
+Future<void> fetchThenPutForecast() async {
+  fetchForecast(http.Client()).then((value) => putForecast(value));
 }
