de6a4142ddac785c3975e681dffc481ec2413e83
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-10-08 17:24:37 +0300

    Add WelcomePage for one time intro

---
+++ lib/widget/NextPageButton.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class NextPageButton extends StatelessWidget {
+
+  final VoidCallback onPressed;
+  final Size size;
+
+  const NextPageButton({Key? key, required this.onPressed, required this.size}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      height: size.height * 0.05,
+      bottom: size.height * 0.05,
+      right: size.width * 0.1,
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text('Next'),
+      ),
+    );
+  }
+}
