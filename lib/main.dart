022a8c2b59edead6b7e60c4f68df383110b3a8b3
Kira Sokolova <Kyra256@proton.me>
2021-10-08 15:56:45 +0300

    Add about screen

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:weather_forecast/utils/MyCacheProvider.dart';
 import 'app/MainApp.dart';
 
 // TODO: add welcome screen where user sets key and city
-// TODO: add about screen
 // TODO: use local stored weather icons, not from network
 
 void main() async {
