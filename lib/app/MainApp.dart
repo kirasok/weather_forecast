c9315e031fb6e17cd247809c8e156213142fc397
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-10-12 13:47:41 +0300

    Add dark theme support for most of app (not include charts)

--- lib/app/MainApp.dart
+++ lib/app/MainApp.dart
@@ -6,11 +6,29 @@ import 'package:weather_forecast/page/WelcomePage.dart';
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context).copyWith(
+    var themeLight = ThemeData.light().copyWith(
       colorScheme: Theme.of(context).colorScheme.copyWith(
-            primary: Colors.lightBlue.shade800,
-            secondary: Colors.deepOrangeAccent.shade700,
+        primary: Colors.lightBlue.shade800,
+        secondary: Colors.deepOrangeAccent.shade700,
+      ),
+      cardTheme: CardTheme.of(context).copyWith(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+        elevation: 5,
+        margin: EdgeInsets.all(8),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(24),
           ),
+        ),
+      ),
+    );
+
+    var themeDark = ThemeData.dark().copyWith(
+      colorScheme: ThemeData.dark().colorScheme.copyWith(
+        secondary: Colors.deepOrangeAccent.shade100,
+      ),
       cardTheme: CardTheme.of(context).copyWith(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
         elevation: 5,
@@ -35,7 +53,9 @@ class MainApp extends StatelessWidget {
 
     return MaterialApp(
       title: 'Weather Forecast',
-      theme: theme,
+      theme: themeLight,
+      darkTheme: themeDark,
+      themeMode: ThemeMode.system,
       home: home,
     );
   }
