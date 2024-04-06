12a83a67a3702e3bb4007b201cb110e73efe4773
Kira Sokolova <Kyra256@proton.me>
2024-04-07 01:34:37 +0300

    fix(build): recreate project to fix platform files

---
+++ android/app/src/main/kotlin/io/github/kirasok/weather_forecast/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.github.kirasok.weather_forecast
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/io/github/kirillmokretsov/weather_forecast/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.github.kirillmokretsov.weather_forecast
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
